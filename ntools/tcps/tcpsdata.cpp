4ca1fccfb14793749821eea95c2ed44cb4c934a3
Alberto Pires <mrpenguin2005@gmail.com>
2014-12-17 13:37:18 -0200

    more code formatting

--- ntools/tcps/tcpsdata.cpp
+++ ntools/tcps/tcpsdata.cpp
@@ -3,14 +3,14 @@
 /* Start   : 01/October/2009                                                 */
 /* Author  : Alberto Pires de Oliveira Neto - email:mrpenguin2005@gmail.com  */
 /*                                                                           */
-/* Last modification : 19/June/2012 by Alberto Pires de Oliveira Neto        */
+/* Last modification : 17/December/2014 by Alberto Pires de Oliveira Neto    */
 /*****************************************************************************/
 
 #include "tcpsdata.h"
 void CapStream::appendData( const struct pcap_pkthdr *header , const u_char *packet ) {
-  Data *d = new Data();
+	Data *d = new Data();
 
-  memcpy( d->packet , packet , header->len );
-  memcpy( (u_char*)&d->header , (u_char*)header , sizeof(struct pcap_pkthdr) );
-  pack.push_back(d);
+	memcpy( d->packet , packet , header->len );
+	memcpy( (u_char*)&d->header , (u_char*)header , sizeof(struct pcap_pkthdr) );
+	pack.push_back(d);
 }
