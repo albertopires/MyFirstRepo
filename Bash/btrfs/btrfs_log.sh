3c83bc0550f653892e45a551c02e37ae9c661a9e
Alberto Pires de Oliveira Neto <mrpenguin2005@gmail.com>
2016-07-28 12:33:17 -0300

    btrfs log messages

--- Bash/btrfs/btrfs_log.sh
+++ Bash/btrfs/btrfs_log.sh
@@ -6,6 +6,10 @@ if [ "$#" -ne 4 ]; then
 	exit
 fi
 
+VERBOSE=2
+
+. ./lib.sh
+
 LOG_FILE=$1
 FROM=$2
 FROM_NAME=$3
@@ -17,17 +21,18 @@ PAST_HOUR_NAME=$LOG_DIR/out_$(date +'%Y%m%d_%H' -d "1 hour ago").log
 
 SEND_MAIL_CMD=../mail/send_mail.sh
 
-#echo $PAST_HOUR
+print_log 2 "$PAST_HOUR"
+
 SUBJECT="BTRFS errors found : $(basename $PAST_HOUR_NAME).xz"
 
 cat $LOG_FILE | grep -i btrfs | egrep "$PAST_HOUR"  |\
 	egrep -i "corrupt|csum" > $PAST_HOUR_NAME
 
 LOG_LINES=`wc -l $PAST_HOUR_NAME | awk '{print $1}'`
-echo "Log lines counted: "$LOG_LINES
+print_log 1 "Log lines counted: "$LOG_LINES
 
 if [ "$LOG_LINES" -gt 0 ]; then
-	echo "Log lines found : "$LOG_LINES
+	print_log 1 "Log lines found : "$LOG_LINES
 	xz $PAST_HOUR_NAME
 	$SEND_MAIL_CMD $FROM "$FROM_NAME" $RCPT "$SUBJECT" $PAST_HOUR_NAME.xz
 fi
---
+++ Bash/btrfs/lib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LOG_VERBOSE=1
+LOG_DEBUG=2
+
+function print_log {
+#echo "Verbose: "$VERBOSE
+#echo "LEVEL  : "$1
+	if (($VERBOSE >= $1)); then
+		echo "$2"
+	fi
+}
+
