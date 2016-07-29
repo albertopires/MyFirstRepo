a4d389e818463dbd5874da1ab7bbffc457a5428d
Alberto Pires de Oliveira Neto <mrpenguin2005@gmail.com>
2016-07-29 15:53:59 -0300

    btrfs: space monitor

---
+++ Bash/btrfs/space_mon.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CMD="cat /tmp/usage.txt"
+
+TMP_FILE="btrfs_us_$$"
+$CMD > $TMP_FILE
+
+DEVICE_SIZE=`cat $TMP_FILE | awk '/Device size\:/ { print $3 }'`
+FREE_ESTIMAT=`cat $TMP_FILE | awk '/Free\ \(estimated\)\:/ { print $3 }'`
+
+echo "Device size    : "$DEVICE_SIZE
+echo "Free Estimated : "$FREE_ESTIMAT
+
+PERCENT_FREE=$(awk "BEGIN { printf \"%.2f\",(100-(${FREE_ESTIMAT}/${DEVICE_SIZE})*100) }")
+echo "Data : "$PERCENT_FREE
+
+rm -f $TMP_FILE
