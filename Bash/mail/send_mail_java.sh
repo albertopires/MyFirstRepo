1d0d73b60e040a2caf073e70b85874fe2bc75a0c
Alberto Pires de Oliveira Neto <alberto.pires.on@gmail.com>
2016-10-17 16:53:30 -0200

    bash sendmail_java wrapper for java-utils

--- Bash/mail/send_mail_java.sh
+++ Bash/mail/send_mail_java.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This is wrapper shell to use java-utils Mail command line tool, when a sendmail
+# is not installed or configured and you have no access to do it.
+# The java-utils artifacts, source code and docs are available at:
+# https://github.com/albertopires/java-utils
+
 export JAVA_HOME="/usr/lib/jvm/jdk1.8.0_45"
 export JAVA="$JAVA_HOME/bin/java"
 export JAVA_UTILS="/tmp/java-utils-0.0.1-jar-with-dependencies.jar"
@@ -16,34 +21,16 @@ RCPT_LIST=$3
 SUBJECT=$4
 FILENAME=$5
 
-echo "From      : "$FROM
-echo "From Name : "$FROM_NAME
-echo "RCPT LIST : "$RCPT_LIST
-echo "Subject   : "$SUBJECT
-echo "File Name : "$FILENAME
-echo 
-
-for i in $RCPT_LIST;
-do
-	echo $i
-done    
+#echo "From      : "$FROM
+#echo "From Name : "$FROM_NAME
+#echo "RCPT LIST : "$RCPT_LIST
+#echo "Subject   : "$SUBJECT
+#echo "File Name : "$FILENAME
+#echo 
 
 PARAM="-f $FILENAME $FROM \"$FROM_NAME\" \"$SUBJECT\" $RCPT_LIST"
-echo "Param: "
-echo $PARAM
+#echo "Param: "
+#echo $PARAM
 
 $JAVA -cp $JAVA_UTILS io.github.albertopires.mail.Example -f $FILENAME $FROM "$FROM_NAME" "$SUBJECT" $RCPT_LIST
-
-#send_mail <from> <from_name> <to> <subject> <file_name>
-
-#Send Mail/File
-#
-#Invalid parameters.
-#-t : send text message.
-#-f : send attached file
-#-m : send text message with attached file
-#
-#-t <msg.txt> <from> <from_name> <subject> <rcpt1> <rcpt2> ... <rcptN>
-#-f <file.any_type> <from> <from_name> <subject> <rcpt1> <rcpt2> ... <rcptN>
-#-m <msg.txt> <file.any_type> <from> <from_name> <subject> <rcpt1> <rcpt2> ... <rcptN>
 
