65ffe0690d1d696113c78209568b1464f713e3e9
Alberto Pires de Oliveira Neto <alberto.pires.on@gmail.com>
2019-01-22 15:39:41 -0200

    ASCII terminal color codes

---
+++ utils/ccodes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo -e "Black  : Esc [30m \033[30m Sample Text \033[0m"
+echo -e "Red    : Esc [31m \033[31m Sample Text \033[0m"
+echo -e "Green  : Esc [32m \033[32m Sample Text \033[0m"
+echo -e "Yellow : Esc [33m \033[33m Sample Text \033[0m"
+echo -e "Blue   : Esc [34m \033[34m Sample Text \033[0m"
+echo -e "Magenta: Esc [35m \033[35m Sample Text \033[0m"
+echo -e "Cyan   : Esc [36m \033[36m Sample Text \033[0m"
+echo -e "White  : Esc [37m \033[37m Sample Text \033[0m"
+
+echo -e "Bright Black  : Esc [30;1m \033[30;1m Sample Text \033[0m"
+echo -e "Bright Red    : Esc [31;1m \033[31;1m Sample Text \033[0m"
+echo -e "Bright Green  : Esc [32;1m \033[32;1m Sample Text \033[0m"
+echo -e "Bright Yellow : Esc [33;1m \033[33;1m Sample Text \033[0m"
+echo -e "Bright Blue   : Esc [34;1m \033[34;1m Sample Text \033[0m"
+echo -e "Bright Magenta: Esc [35;1m \033[35;1m Sample Text \033[0m"
+echo -e "Bright Cyan   : Esc [36;1m \033[36;1m Sample Text \033[0m"
+echo -e "Bright White  : Esc [37;1m \033[37;1m Sample Text \033[0m"
+
+echo -e "Background Bright Red    : Esc [41;1m \033[41;1m Sample Text \033[0m"
+echo -e "Background Bright Green  : Esc [42;1m \033[42;1m Sample Text \033[0m"
+
+echo "Reset: Esc [0m"
